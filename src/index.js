14a333c229eb29c6cd777c5ec54980f5707501fa
Asya19 <58248621+Asya19@users.noreply.github.com>
2022-01-28 20:00:03 +0300

    feat: add function's code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // все скобки вытаскиваем из массива и превращаем в строку
+  bracketsConfig = bracketsConfig.join(',').split(',').join('');
+  
+  let brackets = [],
+      flag = true,
+      actualLength = str.length;
+
+  for (let i = 0; i < bracketsConfig.length; i += 2) {
+    brackets.push(bracketsConfig.slice(i, i + 2));
+  }
+
+  while (flag) {  
+    actualLength = str.length;
+    for (let i = 0; i < brackets.length; i++) {
+      str = str.replace(brackets[i],'');
+    }
+    if (actualLength === str.length)
+      flag = false;
+  }
+  return actualLength === 0;
 }
